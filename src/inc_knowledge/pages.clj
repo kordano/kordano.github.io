544d15daf50aabdc151dd123371abd8806346c5d
kordano <konrad.kuehne@rocketmail.com>
2015-05-11 17:04:29 +0200

    remove clojure solution

--- src/inc_knowledge/pages.clj
+++
@@ -1,1 +0,0 @@
-(ns inc-knowledge.pages)
--- test/inc_knowledge/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns inc-knowledge.core-test
-  (:require [clojure.test :refer :all]
-            [inc-knowledge.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
